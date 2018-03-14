3c93e46e4d6be8633b4ee37d6c85de135a8112ec
Michal <tiredpapi@gmail.com>
2018-03-14 21:38:48 +0000

    jsutils done

--- sandwich-club-starter-code-master/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ sandwich-club-starter-code-master/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -8,17 +8,26 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        Sandwich sandwich;
 
         try {
             JSONObject jsonObject   = new JSONObject(json);
-            sandwich = new Sandwich(jsonObject);
-            Log.i(LogCat.TAG, "TEST: " + jsonObject);
+            JSONObject name = jsonObject.getJSONObject("name");
+
+            String mainName = name.getString("mainName");
+            List<String> alsoKnowAs = Arrays.asList(name.getString("alsoKnownAs"));
+            String placeOfOrigin = jsonObject.getString("placeOfOrigin");
+            String description = jsonObject.getString("description");
+            String image = jsonObject.getString("image");
+            List<String> ingredients = Arrays.asList(jsonObject.getString("ingredients"));
+
+            return new Sandwich(mainName, alsoKnowAs, placeOfOrigin, description, image, ingredients);
         } catch (JSONException e) {
             e.printStackTrace();
         }
